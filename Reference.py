d7bcbb4ce9ca86fd470b0da97e0364dd57212bf9
Jacob Bogner <boginator55@gmail.com>
2021-09-23 21:27:07 -0400

    Implement projectile shot with sound from player on key press, Correct background image so transitions are symmetrical Ready to submit as project 1

--- Reference.py
+++ Reference.py
@@ -1,35 +1,36 @@
 """
-Sprite Collect Coins with Background
+Sprite Bullets
 
 Simple program to show basic sprite usage.
 
 Artwork from https://kenney.nl
 
 If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.sprite_collect_coins_background
+python -m arcade.examples.sprite_bullets
 """
 import random
 import arcade
 import os
 
-PLAYER_SCALING = 0.5
-COIN_SCALING = 0.25
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.2
+SPRITE_SCALING_LASER = 0.8
+COIN_COUNT = 50
 
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Sprite Collect Coins with Background Example"
+SCREEN_TITLE = "Sprites and Bullets Example"
+
+BULLET_SPEED = 5
 
 
 class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+    """ Main application class. """
 
-    def __init__(self, width, height, title):
+    def __init__(self):
         """ Initializer """
-
         # Call the parent class initializer
-        super().__init__(width, height, title)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         # Set the working directory (where we expect to find files) to the same
         # directory this .py file is in. You can leave this out of your own
@@ -38,55 +39,58 @@ class MyGame(arcade.Window):
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
 
-        # Background image will be stored in this variable
-        self.background = None
-
         # Variables that will hold sprite lists
         self.player_list = None
         self.coin_list = None
+        self.bullet_list = None
 
         # Set up the player info
         self.player_sprite = None
         self.score = 0
-        self.score_text = None
 
         # Don't show the mouse cursor
         self.set_mouse_visible(False)
 
-        # Set the background color
+        # Load sounds. Sounds from kenney.nl
+        self.gun_sound = arcade.load_sound(":resources:sounds/hurt5.wav")
+        self.hit_sound = arcade.load_sound(":resources:sounds/hit5.wav")
+
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
 
-        # Load the background image. Do this in the setup so we don't keep reloading it all the time.
-        # Image from:
-        # https://wallpaper-gallery.net/single/free-background-images/free-background-images-22.html
-        self.background = arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
+        """ Set up the game and initialize the variables. """
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         # Set up the player
         self.score = 0
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
-                                           PLAYER_SCALING)
+
+        # Image from kenney.nl
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.center_y = 70
         self.player_list.append(self.player_sprite)
 
-        for i in range(50):
+        # Create the coins
+        for i in range(COIN_COUNT):
 
             # Create the coin instance
-            coin = arcade.Sprite(":resources:images/items/coinGold.png", COIN_SCALING)
+            # Coin image from kenney.nl
+            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
 
             # Position the coin
             coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
+            coin.center_y = random.randrange(120, SCREEN_HEIGHT)
 
             # Add the coin to the lists
             self.coin_list.append(coin)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def on_draw(self):
         """
@@ -96,13 +100,9 @@ class MyGame(arcade.Window):
         # This command has to happen before we start drawing
         arcade.start_render()
 
-        # Draw the background texture
-        arcade.draw_lrwh_rectangle_textured(0, 0,
-                                            SCREEN_WIDTH, SCREEN_HEIGHT,
-                                            self.background)
-
         # Draw all the sprites.
         self.coin_list.draw()
+        self.bullet_list.draw()
         self.player_list.draw()
 
         # Render the text
@@ -113,27 +113,61 @@ class MyGame(arcade.Window):
         Called whenever the mouse moves.
         """
         self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called whenever the mouse button is clicked.
+        """
+        # Gunshot sound
+        arcade.play_sound(self.gun_sound)
+        # Create a bullet
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
+
+        # The image points to the right, and we want it to point up. So
+        # rotate it.
+        bullet.angle = 90
+
+        # Give the bullet a speed
+        bullet.change_y = BULLET_SPEED
+
+        # Position the bullet
+        bullet.center_x = self.player_sprite.center_x
+        bullet.bottom = self.player_sprite.top
+
+        # Add the bullet to the appropriate lists
+        self.bullet_list.append(bullet)
 
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        # Call update on the coin sprites (The sprites don't do much in this
-        # example though.)
-        self.coin_list.update()
+        # Call update on bullet sprites
+        self.bullet_list.update()
 
-        # Generate a list of all sprites that collided with the player.
-        hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+        # Loop through each bullet
+        for bullet in self.bullet_list:
 
-        # Loop through each colliding sprite, remove it, and add to the score.
-        for coin in hit_list:
-            coin.remove_from_sprite_lists()
-            self.score += 1
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet, self.coin_list)
+
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                bullet.remove_from_sprite_lists()
+
+            # For every coin we hit, add to the score and remove the coin
+            for coin in hit_list:
+                coin.remove_from_sprite_lists()
+                self.score += 1
+
+                # Hit Sound
+                arcade.play_sound(self.hit_sound)
+
+            # If the bullet flies off-screen, remove it.
+            if bullet.bottom > SCREEN_HEIGHT:
+                bullet.remove_from_sprite_lists()
 
 
 def main():
-    """ Main function """
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = MyGame()
     window.setup()
     arcade.run()
 
--- ReySuperScroller.py
+++ ReySuperScroller.py
@@ -1,12 +1,25 @@
 """
-This super scroller game lets the player manipulate Rey the fox as he shoots his pellet gun at absolutely nothing.
-He can move up or down as he runs along the map.
+This super scroller game lets the player manipulate Rey the fox as he shoots
+his laser gun at absolutely nothing.
+He can move up or down as the map scrolls.
+Simply run ReySuperScroller.py after cloning and ensure that arcade is installed.
+
+Written by Jacob Bogner for 2D Game Design project 1 (due 9/23/2021)
 
 The base code is built off the code provided in-class.
 
-The moving background code was built from an example found here:
+The moving background code was built using an example found here:
 https://stackoverflow.com/questions/55167496/infinite-scrolling-background-in-python
 
+The projectile-shooting functionality was built using an example found here:
+https://api.arcade.academy/en/latest/examples/sprite_bullets.html#sprite-bullets
+
+Sound is provided by Freesound at the following link:
+https://freesound.org/people/sunnyflower/sounds/361471/
+
+All artwork (c) Jack Brady (a friend of mine who has gladly granted
+me permission to use any of his original artwork in any game I create
+this semester)
 
 """
 import arcade
@@ -17,6 +30,8 @@ SCREEN_WIDTH = 1821
 SCREEN_HEIGHT = 1024
 BG_WIDTH = 1821
 SCROLLRATE = 3
+BULLET_SPEED = 10
+BULLET_OFFSET = 65  # Rey's gun is about 80 pixels up from the bottom of the .png
 
 
 class MoveEnum(Enum):
@@ -48,12 +63,15 @@ class MinimalSprite(arcade.Sprite):
 
 
 class MimimalArcade(arcade.Window):
+    """Main application class"""
 
     def __init__(self, image_name: str, SCREEN_WIDTH, SCREEN_HEIGHT):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.image_path = pathlib.Path.cwd() / 'assets' / image_name
         self.bga_path = pathlib.Path.cwd() / 'assets' / 'BGA.png'
         self.bgb_path = pathlib.Path.cwd() / 'assets' / 'BGB.png'
+        self.bulletPNG_path = pathlib.Path.cwd() / 'assets' / 'bullet.png'
+        self.bulletWAV_path = pathlib.Path.cwd() / 'assets' / 'laser_shot.wav'
         self.pict = None
         self.direction = MoveEnum.NONE
         self.pictlist = None
@@ -61,30 +79,30 @@ class MimimalArcade(arcade.Window):
         self.background_sprite = arcade.Sprite(self.bga_path)
         self.background_list = arcade.SpriteList()
 
+        self.bullet_list = None
+        self.set_mouse_visible(False)
+        self.gun_sound = arcade.load_sound(self.bulletWAV_path)
+
     def setup(self):
         # SETUP THE PLAYER SPRITE:
         self.pict = MinimalSprite(str(self.image_path), speed=3, game_window=self)
         self.pict.center_x = 500
         self.pict.center_y = 500
         self.pictlist = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
         self.pictlist.append(self.pict)
 
         # SETUP THE BACKGROUND IMAGES:
-
         self.background_sprite.center_x = BG_WIDTH // 2
         self.background_sprite.center_y = SCREEN_HEIGHT // 2
         self.background_sprite.change_x = -SCROLLRATE
-
         self.background_list.append(self.background_sprite)
 
         # SECOND BACKGROUND IMAGE
-
         self.background_sprite_2.center_x = SCREEN_WIDTH + BG_WIDTH // 2
         self.background_sprite_2.center_y = SCREEN_HEIGHT // 2
         self.background_sprite_2.change_x = -SCROLLRATE
-
         self.background_list.append(self.background_sprite_2)
-
 
     def on_draw(self):
         """ Render the screen. """
@@ -92,6 +110,7 @@ class MimimalArcade(arcade.Window):
         # Code to draw the screen goes here
         self.background_list.draw()
         self.pictlist.draw()
+        self.bullet_list.draw()
 
     def update(self, delta_time: float):
         # to get really smooth movement we would use the delta time to
@@ -102,10 +121,12 @@ class MimimalArcade(arcade.Window):
 
         if self.background_sprite_2.right < 0:
             self.background_sprite_2.center_x = self.background_sprite.center_x + BG_WIDTH
-
         self.background_list.update()
-
-
+        self.bullet_list.update()
+        # Loop through each bullet
+        for bullet in self.bullet_list:
+            if bullet.right > SCREEN_WIDTH:
+                bullet.remove_from_sprite_lists()
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -113,12 +134,14 @@ class MimimalArcade(arcade.Window):
             self.direction = MoveEnum.UP
         elif key == arcade.key.DOWN or key == arcade.key.S:
             self.direction = MoveEnum.DOWN
-        """
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.direction = MoveEnum.LEFT
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.direction = MoveEnum.RIGHT
-        """
+        elif key == arcade.key.SPACE:
+            arcade.play_sound(self.gun_sound)
+            bullet = arcade.Sprite(self.bulletPNG_path)
+            bullet.change_x = BULLET_SPEED
+            bullet.center_x = self.pict.right
+            bullet.bottom = self.pict.bottom + BULLET_OFFSET
+            self.bullet_list.append(bullet)
+
     def on_key_release(self, key: int, modifiers: int):
         """called by arcade for keyup events"""
         if (key == arcade.key.UP or key == arcade.key.W) and \
